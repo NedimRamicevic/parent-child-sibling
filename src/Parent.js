e2a33f171abc3349ea49d55b374da8ca112997c9
Nedim <mr.ramicevic@gmail.com>
2021-08-20 14:15:53 +0300

    parent

---
+++ src/Parent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Child } from './Child';
+import { Sibling } from './Sibling';
+
+export class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Child 
+          name={this.state.name} 
+          onChange={this.changeName} />
+        <Sibling />
+      </div>
+    );
+  }
+}
